063d19dad9bed9f2089521d2530d0192137e8cdc
张豪 <739415161@qq.com>
2018-07-06 14:49:32 +0800

    Add files via upload

---
+++ 在线音乐保存到沙盒/在线音乐保存到沙盒/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  在线音乐保存到沙盒
+//
+//  Created by ZZZZZ on 15/11/23.
+//  Copyright (c) 2015年 张豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 在线音乐保存到沙盒/在线音乐保存到沙盒/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  在线音乐保存到沙盒
+//
+//  Created by ZZZZZ on 15/11/23.
+//  Copyright (c) 2015年 张豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
